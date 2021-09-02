62c2feb329fe85be988ae627bd68a9c78b5e5ad0
Calebe Costa <calebe@clickativo.com.br>
2021-09-02 16:16:45 -0300

    limpeza e ajuste de codigo

--- src/containers/agendamento/DataAgendamento.js
+++ src/containers/agendamento/DataAgendamento.js
@@ -1,78 +1,66 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-no-undef */
-import react,{useState} from 'react';
-import DatePicker,{registerLocale} from 'react-datepicker';
+import react, { useState } from 'react';
+import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import pt from "date-fns/locale/pt-BR";
+import pt from 'date-fns/locale/pt-BR';
 import setHours from 'date-fns/setHours';
 import setMinutes from 'date-fns/setMinutes';
 import moment from 'moment';
-import {
-    Row,
-    Card,
-    CardBody,
-    Input,
-    FormGroup,
-    Label,
-    Button,
-    Form,
-    Nav,
-  
-  } from 'reactstrap';
+import { FormGroup, Label } from 'reactstrap';
 import { Colxx } from '../../components/common/CustomBootstrap';
 
-registerLocale("pt", pt)
-moment.locale('pt') 
+registerLocale('pt', pt);
+moment.locale('pt');
 
+const DataAgendamento = () => {
+  const isWeekday = (date: Date) => {
+    const day = date.getDay(date);
+    return day !== 0 && day !== 6;
+  };
 
-const DataAgendamento = ( ) => { 
-
-    const isWeekday = (date: Date) => {
-        const day = date.getDay(date)
-        return day !== 0 && day !== 6
-      }
-
-    const filterPassedTime = (time) => {
+  const filterPassedTime = (time) => {
     const currentDate = new Date();
     const selectedDate = new Date(time);
-
     return currentDate.getTime() < selectedDate.getTime();
-    };
+  };
 
-    const [option,setOption] = useState('');
-    const [dt_inicio, setDtInicio] = useState('');
+  const [option] = useState('');
+  const [dt_inicio, setDtInicio] = useState('');
 
-    const onChageDtAgendamento = (e) => {
-        setDtInicio(e)
-        const data = JSON.parse(localStorage.getItem("agendamento"));
-        data.data = e;
-        localStorage.setItem("agendamento",JSON.stringify(data));
-    }
-    
-return (
+  const onChageDtAgendamento = (e) => {
+    setDtInicio(e);
+    const data = JSON.parse(localStorage.getItem('agendamento'));
+    data.data = e;
+    localStorage.setItem('agendamento', JSON.stringify(data));
+  };
+
+  return (
     <>
-    <Colxx sm={6}>
-    <FormGroup>
-    <Label for="colaborador">Data de Agendamento</Label>
-    <DatePicker
-    disabled={option}
-        onChangeRaw={(e)=> e.preventDefault()}
-        showTimeSelect
-        locale='pt'
-        dateFormat="dd/MM/yyyy HH:mm"
-        minDate={moment().toDate()}
-        minTime={setHours(setMinutes(new Date(), 0), 8)}
-        maxTime={setHours(setMinutes(new Date(), 30), 18)}
-        timeIntervals={60}
-        filterDate={isWeekday}
-        filterTime={filterPassedTime}
-        selected={dt_inicio}
-        onChange={(e)=>{onChageDtAgendamento(e)}}
-        placeholderText="Selecione uma data"
-    />
-    </FormGroup>
-    </Colxx>
+      <Colxx sm={6}>
+        <FormGroup>
+          <Label for="colaborador">Data de Agendamento</Label>
+          <DatePicker
+            disabled={option}
+            onChangeRaw={(e) => e.preventDefault()}
+            showTimeSelect
+            locale="pt"
+            dateFormat="dd/MM/yyyy HH:mm"
+            minDate={moment().toDate()}
+            minTime={setHours(setMinutes(new Date(), 0), 8)}
+            maxTime={setHours(setMinutes(new Date(), 30), 18)}
+            timeIntervals={60}
+            filterDate={isWeekday}
+            filterTime={filterPassedTime}
+            selected={dt_inicio}
+            onChange={(e) => {
+              onChageDtAgendamento(e);
+            }}
+            placeholderText="Selecione uma data"
+          />
+        </FormGroup>
+      </Colxx>
     </>
-    );
-    };
+  );
+};
 export default DataAgendamento;
